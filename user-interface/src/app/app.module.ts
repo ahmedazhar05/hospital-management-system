055bed8d94a8b3f6f54ab3d08aea86645d19fba2
Vish7279 <122888439+Vish7279@users.noreply.github.com>
2023-02-13 18:31:03 +0530

    created prescription page
    #17

--- user-interface/src/app/app.module.ts
+++ user-interface/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PrescriptionComponent } from './prescription/prescription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PrescriptionComponent
   ],
   imports: [
     BrowserModule,
---
+++ user-interface/src/app/prescription/prescription.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prescription',
+  templateUrl: './prescription.component.html',
+  styleUrls: ['./prescription.component.css']
+})
+export class PrescriptionComponent {
+  address = "Hospital, Mumbai, Matoshree Nagar, Nashik, Maharashtra 422002"
+  mob_no = "8087030000"
+  phn_no = "1234567891"
+  mail_id = "ahmedazhar@gmail.com"
+  name = "Dr. kuber kanade"
+  degree = "MBBS, DNB(Gen. Surgeon), MS, MNAMS, FMAS, FIAGES, FALS, AFIH"
+
+}
