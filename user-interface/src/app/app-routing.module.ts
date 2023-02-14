e4684dc401542f9b206a6787f9aca6f2c353d750
Vish7279 <122888439+Vish7279@users.noreply.github.com>
2023-02-14 16:10:41 +0530

    created signup page
    #20

--- user-interface/src/app/app-routing.module.ts
+++ user-interface/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     title: 'Health+ Login',
     path: 'login',
     component: LoginComponent
+  },
+  {
+    title: 'Health+ Signup',
+    path: 'register',
+    component: SignupComponent
   },
   {
     title: 'Health+ Home',
