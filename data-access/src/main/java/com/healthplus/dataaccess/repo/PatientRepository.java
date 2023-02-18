2e168d0999bdc9a3f7805190727d5dd1fa92b8fb
Azhar Ahmed <ahmedazhar05@yahoo.com>
2023-02-18 16:02:17 +0530

    changed from CrudRepo to JpaRepo & used native query

--- data-access/src/main/java/com/healthplus/dataaccess/repo/PatientRepository.java
+++ data-access/src/main/java/com/healthplus/dataaccess/repo/PatientRepository.java
@@ -2,17 +2,16 @@ package com.healthplus.dataaccess.repo;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.healthplus.dataaccess.domain.Patient;
 
-public interface PatientRepository extends CrudRepository<Patient, Integer> {
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
-	@Query("SELECT * FROM Patient WHERE email = ?1")
+	@Query(value="SELECT * FROM patient WHERE email = ?1", nativeQuery=true)
 	public Optional<Patient> getPatientByEmail(String email);
 
-	@Query("SELECT * FROM Patient WHERE contact = ?1")
+	@Query(value="SELECT * FROM patient WHERE contact = ?1", nativeQuery=true)
 	public Optional<Patient> getPatientByContact(Long contact);
 }
