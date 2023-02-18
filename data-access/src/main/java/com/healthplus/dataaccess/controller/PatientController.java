bbc020c65b562683b76cc646f40c9ada494b0ede
Azhar Ahmed <ahmedazhar05@yahoo.com>
2023-02-18 16:18:40 +0530

    Redesigned API endpoints & changed controller accordingly

--- data-access/src/main/java/com/healthplus/dataaccess/controller/PatientController.java
+++ data-access/src/main/java/com/healthplus/dataaccess/controller/PatientController.java
@@ -1,10 +1,12 @@
 package com.healthplus.dataaccess.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,27 +14,27 @@ import com.healthplus.dataaccess.domain.Patient;
 import com.healthplus.dataaccess.repo.PatientRepository;
 
 @Controller
-@RequestMapping(path="/patient")
+@RequestMapping(path="/patients")
 public class PatientController {
 	@Autowired
 	private PatientRepository patientRepository;
 	
 	@GetMapping(path="/")
-	public Iterable<Patient> listPatients(){
+	public List<Patient> listPatients(){
 		return patientRepository.findAll();
 	}
 	
-	@GetMapping(path="/get")
-	public Optional<Patient> getPatientBy(@RequestParam("id") Integer id) {
-		return patientRepository.findById(id);
+	@GetMapping(path="/{id}")
+	public List<Patient> getPatientBy(@PathVariable("id") Integer id){
+		return patientRepository.findAll();
 	}
 	
-	@GetMapping(path="/get")
+	@GetMapping(path="/search", params={"email"})
 	public Optional<Patient> getPatientBy(@RequestParam("email") String email){
 		return patientRepository.getPatientByEmail(email);
 	}
 	
-	@GetMapping(path="/get")
+	@GetMapping(path="/search", params={"contact"})
 	public Optional<Patient> getPatientBy(@RequestParam("contact") Long contact){
 		return patientRepository.getPatientByContact(contact);
 	}
