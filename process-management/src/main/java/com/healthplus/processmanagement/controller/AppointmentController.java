d43af0059624c0f73832c0af0c608686dab59eb4
Kuber-Kanade <kuberkanade85@gmail.com>
2023-02-21 03:16:42 +0530

    Created process control classes
    1. BillController
    2. AppointmentController

--- process-management/src/main/java/com/healthplus/processmanagement/controller/AppointmentController.java
+++ process-management/src/main/java/com/healthplus/processmanagement/controller/AppointmentController.java
@@ -6,16 +6,23 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.healthplus.dataaccess.domain.Appointment;
 import com.healthplus.processmanagement.model.Patient;
 import com.healthplus.processmanagement.model.Report;
 
 @RestController
 public class AppointmentController {
 	private final String REPORT_URI = "http://localhost:8080/reports";
+	private final String APPOINTMENT_URI = "http://localhost:8080/appointments";
+	private final String PATIENT_URI = "http://localhost:8080/patients";
 	@Autowired
 	private RestTemplate restTemplate;
 	
@@ -31,5 +38,27 @@ public class AppointmentController {
 		}
 		return m;
 	}
+	
+/*	@PostMapping(path = "/")
+    public @ResponseBody String addNewAppointment(@RequestBody Appointment newAppointment) {
+        appointmentRepository.save(newAppointment);
+        return "Added";
+    } */
+	
+	
+	@GetMapping(path="appointment/schedule/{id}")
+	public Map<String, Object> getScheduleDefaults(@PathVariable("id") Long appointment){
+	
+		com.healthplus.processmanagement.model.Appointment a;
+		a=restTemplate.getForObject(APPOINTMENT_URI+"/"+appointment,Appointment.class);
+		
+		Patient p;
+		p=restTemplate.getForObject(PATIENT_URI+"/"+a.getPatient(),Patient.class);
+		
+		Report r;
+		r=restTemplate.getForObject(REPORT_URI+"/search?patient="+p.getId(),Report.class);
+		
+	}
+	
 
 }
--- process-management/src/main/java/com/healthplus/processmanagement/controller/BillController.java
+++ process-management/src/main/java/com/healthplus/processmanagement/controller/BillController.java
@@ -1,5 +1,84 @@
 package com.healthplus.processmanagement.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.healthplus.processmanagement.model.Doctor;
+import com.healthplus.processmanagement.model.MedicinePlan;
+import com.healthplus.processmanagement.model.Patient;
+import com.healthplus.processmanagement.model.Prescription;
+
+@RestController
 public class BillController {
+
+	private final String BILL_URI = "http://localhost:8080/bills";
+	private final String PATIENT_URI = "http://localhost:8080/patients";
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping(path = "bill/getPrescriptions", params = { "credential" })
+	public List<Prescription> getPrescriptionDefaults(@RequestParam("credential") String credential) {
+		Patient p;
+		if (Pattern.matches("[A-Za-z0-9_.]+@[A-Za-z]+\\.[A-Za-z]+", credential)) {
+			p = restTemplate.getForObject(PATIENT_URI + "/search?email=" + credential, Patient.class);
+		}
+
+		else {
+			p = restTemplate.getForObject(PATIENT_URI + "/search?contact=" + credential, Patient.class);
+		}
+
+		if (p == null) {
+			// TODO: Create HTTP Response and return
+		}
+
+		else {
+			List<Prescription> pr = restTemplate.getForObject(PATIENT_URI + "/" + p.getId() + "/prescriptions",
+					Prescription.class);
+			return pr;
+		}
+		return new ArrayList(0);
+	}
+	
+	
+	private final String MEDICINE_URI = "http://localhost:8080/medicines";
+	private final String PRESCRIPTION_URI = "http://localhost:8080/prescriptions";
+	private final String DOCTOR_URI = "http://localhost:8080/doctors";
+	@GetMapping(path = "/bill/prescription/{id}")
+	
+	public Map<String, Integer> getPrescription(@PathVariable("id") Long prescription){
+		/*    Output:    {        "medicines_cost" : 123        "professional_fees": 213    }    */
+		List<MedicinePlan> medicines;
+		
+		medicines=restTemplate.getForObject(MEDICINE_URI + "/search?prescription=" + prescription, MedicinePlan.class);
+		
+		int count=medicines.size();
+		
+		 int medTotal=count*100;
+		 
+		 Prescription pr;
+		 pr=restTemplate.getForObject(PRESCRIPTION_URI + "/" + prescription, Prescrption.class);
+		 
+		 Doctor dr;
+		 dr=restTemplate.getForObject(DOCTOR_URI + "/" + pr.getDoctor(), Doctor.class);
+		 int fee=dr.getFees();
+		 Map <String, Object> t = new HashMap();
+		 t.put("Medicines",count );
+		 t.put("Professional fees",fee );
+		 }
+	
+	
+	
+	
 
 }
