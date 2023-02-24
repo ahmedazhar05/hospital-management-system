2e0f63e29a26cca954608714891ba21bf7875f08
Azhar Ahmed <ahmedazhar05@yahoo.com>
2023-02-25 02:57:34 +0530

    bunch of major updates

---
+++ user-interface/src/app/utilities/utility.ts
@@ -0,0 +1,10 @@
+export class Utilities {
+
+    formatDate(date: Date){
+        return date.toLocaleString('en-US', {
+            year:'numeric',
+            month:'2-digit',
+            day:'2-digit'
+        }).split('/').reverse().join('-');
+    }
+}
